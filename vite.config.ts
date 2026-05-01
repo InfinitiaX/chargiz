c36a1f014e2d5240ecfe3606a926b72df6e608cb
FlexyAyou <b.ndiaye@reselform.fr>
2026-05-01 20:35:09 +0000

    Add tailwind plugin to restore styles

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
