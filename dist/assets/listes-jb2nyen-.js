bab874c879bee9af215e4136d0e32f6d0cbc8830
FlexyAyou <b.ndiaye@reselform.fr>
2026-05-03 11:36:49 +0000

    feat: add production dist build

---
+++ dist/assets/listes-jb2nyen-.js
@@ -0,0 +1,1 @@
+import{j as t,O as o}from"./vendor-router-D33zPnDO.js";function e(){return t.jsx(o,{})}export{e as component};
---
+++ dist/assets/logo-chargiz-Bsl1pN-Y.js
@@ -0,0 +1,1 @@
+const o="/assets/logo-chargiz-iBnNZ_0F.png";export{o as l};
