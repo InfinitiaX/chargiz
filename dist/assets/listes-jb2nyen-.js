fba6c807d15cade69cfd72f98215dd34c59bde5d
FlexyAyou <b.ndiaye@reselform.fr>
2026-05-04 08:47:40 +0000

    security: re-add dist to .gitignore and remove from git tracking

--- dist/assets/listes-jb2nyen-.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./vendor-router-D33zPnDO.js";function e(){return t.jsx(o,{})}export{e as component};
--- dist/assets/logo-chargiz-Bsl1pN-Y.js
+++
@@ -1,1 +0,0 @@
-const o="/assets/logo-chargiz-iBnNZ_0F.png";export{o as l};
